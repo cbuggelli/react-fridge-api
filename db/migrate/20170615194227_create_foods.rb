3974c465abe3a789f14c3b5c642ee0373b8ddf33
github username <github email address>
2017-06-15 16:26:21 -0400

    database added

---
+++ db/migrate/20170615194227_create_foods.rb
@@ -0,0 +1,12 @@
+class CreateFoods < ActiveRecord::Migration[5.1]
+  def change
+    create_table :foods do |t|
+      t.string :name
+      t.integer :days_until_expiration
+      t.string :quantity
+      t.integer :category_id
+
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Category.delete_all
+puts "seeding categories"
+meat = Category.create(name: "meat")
+produce = Category.create(name: "produce")
+dairy = Category.create(name: "dairy")
+baked = Category.create(name: "baked")
+puts "seeding categories complete"
+
+puts "seeding foods"
+chicken = Food.create(name: "Chicken", days_until_expiration: 2, category_id: 1, quantity: "1")
+carrot = Food.create(name: "Carrot", days_until_expiration: 2, quantity: "1", category_id: 2)
+beef = Food.create(name: "Beef", days_until_expiration: 5, category_id: 1, quantity: "2")
+celery = Food.create(name: "Celery", days_until_expiration: 1, category_id: 2, quantity: "5")
+milk = Food.create(name: "Milk", days_until_expiration: 7, category_id: 3, quantity: "1")
+bread = Food.create(name: "Bread", days_until_expiration: 10, category_id: 4, quantity: "1")
+puts "seeding foods complete"
