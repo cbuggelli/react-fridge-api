361b90da3d2a506b8ccba6853290531b768772eb
github username <github email address>
2017-06-16 17:12:48 -0400

    fixed cors

--- app/controllers/api/v1/foods_controller.rb
+++ app/controllers/api/v1/foods_controller.rb
@@ -5,12 +5,19 @@ class Api::V1::FoodsController < ApplicationController
   end
 
   def create
-    
+    food = Food.create(student_params)
+    render json: food
   end
 
   def show
     @food = Food.find_by(id: params[:id])
     render json: @food
+  end
+
+
+  private
+  def student_params
+    params.require(:food).permit(:name, :days_until_expiration, :quantity, :category_id)
   end
 
 end
