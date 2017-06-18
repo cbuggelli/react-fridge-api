b466dd1ebe4db48b6cd6da50d98fdf45841e662c
github username <github email address>
2017-06-18 10:19:52 -0400

    changed days left to expiration date

--- db/migrate/20170615194227_create_foods.rb
+++ db/migrate/20170615194227_create_foods.rb
@@ -2,7 +2,7 @@ class CreateFoods < ActiveRecord::Migration[5.1]
   def change
     create_table :foods do |t|
       t.string :name
-      t.integer :days_until_expiration
+      t.string :expiration_date
       t.string :quantity
       t.integer :category_id
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -14,10 +14,10 @@ baked = Category.create(name: "baked")
 puts "seeding categories complete"
 
 puts "seeding foods"
-chicken = Food.create(name: "Chicken", days_until_expiration: 2, category_id: 1, quantity: "1")
-carrot = Food.create(name: "Carrot", days_until_expiration: 2, quantity: "1", category_id: 2)
-beef = Food.create(name: "Beef", days_until_expiration: 5, category_id: 1, quantity: "2")
-celery = Food.create(name: "Celery", days_until_expiration: 1, category_id: 2, quantity: "5")
-milk = Food.create(name: "Milk", days_until_expiration: 7, category_id: 3, quantity: "1")
-bread = Food.create(name: "Bread", days_until_expiration: 10, category_id: 4, quantity: "1")
+chicken = Food.create(name: "Chicken", expiration_date: "2017-07-08", category_id: 1, quantity: "1")
+carrot = Food.create(name: "Carrot", expiration_date: "2017-07-06", quantity: "1", category_id: 2)
+beef = Food.create(name: "Beef", expiration_date: "2017-07-05", category_id: 1, quantity: "2")
+celery = Food.create(name: "Celery", expiration_date: "2017-07-05", category_id: 2, quantity: "5")
+milk = Food.create(name: "Milk", expiration_date: "2017-07-05", category_id: 3, quantity: "1")
+bread = Food.create(name: "Bread", expiration_date: "2017-07-05", category_id: 4, quantity: "1")
 puts "seeding foods complete"
