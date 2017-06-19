88bfb9710a104d0178f6eb20335c84b919a2c87b
github username <github email address>
2017-06-19 15:33:59 -0400

    ch ch ch changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :foods, only: [:index, :show, :create]
+      resources :foods
       resources :categories, only: [:index]
     end
   end
